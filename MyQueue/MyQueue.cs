21a7b40bb7396d3e6d27b8ebfe7e07be7c0fe979
BacDay32 <lephuocbac@gmail.com>
2020-12-07 17:46:00 +0700

    first commit

---
+++ MyQueue/MyQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    class MyQueue<T>
+    {
+        public int topIndex { get; set; } = -1;
+        private T valueQueue { get; set; } = default(T);
+        public List<T> list { get; set; } = new List<T>();
+        public bool IsEmpty()
+        {
+            if(list.Count==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public T DeQueue()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("<<<<<Queue is empty>>>>>");
+                Console.ReadLine();
+            }
+            T item = list[0];
+            list.Remove(item);
+            return item;                  
+        }
+        public void EnQueue(T valueQueue)
+        {
+            Console.WriteLine("Enter value QUEUE>>>>>");
+            list.Add(valueQueue);
+        } 
+        public T Front()
+        {
+            return list[0];
+        }
+    }
+}
---
+++ MyQueue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<string> st = new MyQueue<string>();
+            while(true)
+            {
+                Console.WriteLine("what do you want with QUEUE? :");
+                Console.WriteLine("1: EnQueue");
+                Console.WriteLine("2: DeQueue");
+                Console.WriteLine("3: Front");
+                int num = int.Parse(Console.ReadLine());
+                switch(num)
+                {
+                    case 1:
+                        Console.WriteLine("Enter value QUEUE: ");                       
+                        string valueQueue = Console.ReadLine();
+                        st.EnQueue(valueQueue);
+                        break;
+                    case 2:
+                        Console.WriteLine("<<<Remove>>> "+ st.DeQueue() +" <<<in QUEUE>>>");
+                        Console.WriteLine("Enter to continue");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Value front in QUEUE is:>>" + st.Front());
+                        Console.WriteLine("Enter to continue");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }   
+    }
+}
